a151ec15dc213cebd0844add80e28defc5e7329e
vachiravit klaiklaw <earthgodna@gmail.com>
2022-04-25 12:04:03 +0700

    test

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -16,6 +16,7 @@ export class LoginComponent implements OnInit {
   loginForms:Array<Loginf>  =[];
 
   constructor(private router:Router, private fb:FormBuilder,private loginservice:LoginService) {
+    this.logindata = new Loginf("6204062616103","1129700214653")
     this.loginForm = this.fb.group({
       Username: [''],
       Password: ['']
@@ -28,7 +29,7 @@ export class LoginComponent implements OnInit {
 
   onSubmit(f:FormGroup){
     //Bug cannot find username
-    // this.logindata.username = f.get('Username')?.value
+    this.logindata.username = f.get('Username')?.value
 
     console.log(f.get('Username')?.value)
     console.log(f.get('Password')?.value)
