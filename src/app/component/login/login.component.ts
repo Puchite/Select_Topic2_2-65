99c8cc2632df1bf1555663ac91335a34f0efd12b
vachiravit klaiklaw <earthgodna@gmail.com>
2022-04-26 01:08:56 +0700

    check user and password

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import {Loginf} from "./loginf"
 import {Router} from "@angular/router"
 import { UserService } from 'src/app/service/user.service';
+import {HttpClient} from '@angular/common/http'
 
 // import { LoginService } from '../Service/loginservice.service';
 @Component({
@@ -15,8 +16,9 @@ export class LoginComponent implements OnInit {
   logindata!: Loginf;
   loginForm!: FormGroup;
   loginForms:Array<Loginf>  =[];
-
-  constructor(private router:Router, private fb:FormBuilder,private loginservice:LoginService, private userservice:UserService) {
+  Data:Array<any> =[];
+  state:string =" ";
+  constructor(private router:Router, private fb:FormBuilder,private loginservice:LoginService, private userservice:UserService,private http:HttpClient) {
     this.logindata = new Loginf("6204062616103","1129700214653")
     this.loginForm = this.fb.group({
       Username: [''],
@@ -26,16 +28,36 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.userservice.login(this.logindata.username, this.logindata.password).subscribe(data=>{
+      this.Data.push(data)
+      this.Data = this.Data[0]
+
+    })
   }
 
   onSubmit(f:FormGroup){
     //Bug cannot find username
     this.logindata.username = f.get('Username')?.value
     this.logindata.password = f.get('Password')?.value
-    console.log(f.get('Username')?.value)
-    console.log(f.get('Password')?.value)
-    console.log(this.userservice.login(this.logindata.username, this.logindata.password))
-    this.router.navigate(['/Home'])
-  }
 
+
+    // console.log(this.Data)
+    if(f.get('Username')?.value==""){
+      this.state = "please enter your username"
+    }
+    else if(f.get('Password')?.value==""){
+      this.state = "please enter your password"
+    }else {
+      for(let i = 0;i<this.Data.length;i++){
+        if(this.logindata.username === this.Data[i].Student_ID&&this.logindata.password===this.Data[i].Password){
+            this.router.navigate(['/Home'])
+        }
+        else{
+          this.state = "Incorrect username or password.Please try again."
+        }
+      }
+
+    // this.router.navigate(['/Home'])
+   }
+  }
 }
