43db8cac3256c68cc753a1af02f3fde781ac763a
Puchite <puchitekub12@gmail.com>
2022-04-27 17:58:20 +0700

    login with nestjs

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -2,9 +2,10 @@ import { LoginService } from './../Service/loginservice.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import {Loginf} from "./loginf"
-import {Router} from "@angular/router"
+import {ActivatedRoute, Router} from "@angular/router"
 import { UserService } from 'src/app/service/user.service';
 import {HttpClient} from '@angular/common/http'
+import { first } from 'rxjs/operators';
 
 // import { LoginService } from '../Service/loginservice.service';
 @Component({
@@ -18,7 +19,13 @@ export class LoginComponent implements OnInit {
   loginForms:Array<Loginf>  =[];
   Data:Array<any> =[];
   state:string =" ";
-  constructor(private router:Router, private fb:FormBuilder,private loginservice:LoginService, private userservice:UserService,private http:HttpClient) {
+  constructor(
+    private router:Router,
+    private fb:FormBuilder,
+    private loginservice:LoginService,
+    private userservice:UserService,
+    private http:HttpClient,
+    private route: ActivatedRoute) {
     this.logindata = new Loginf("6204062616103","1129700214653")
     this.loginForm = this.fb.group({
       Username: [''],
@@ -28,45 +35,46 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.userservice.login(this.logindata.username, this.logindata.password).subscribe(data=>{
-      this.Data.push(data)
-      this.Data = this.Data[0]
 
-    })
   }
 
   onSubmit(f:FormGroup){
     //Bug cannot find username
     this.logindata.username = f.get('Username')?.value
     this.logindata.password = f.get('Password')?.value
-<<<<<<< HEAD
-    console.log(f.get('Username')?.value)
-    console.log(f.get('Password')?.value)
-    let data:any = (this.userservice.login(this.logindata.username, this.logindata.password))
-    console.log(data)
-    this.router.navigate(['/Home'])
-  }
-=======
->>>>>>> 19b93a534573bd883bda03df41b70eb3801b892c
 
 
     // console.log(this.Data)
-    if(f.get('Username')?.value==""){
-      this.state = "please enter your username"
-    }
-    else if(f.get('Password')?.value==""){
-      this.state = "please enter your password"
-    }else {
-      for(let i = 0;i<this.Data.length;i++){
-        if(this.logindata.username === this.Data[i].Student_ID&&this.logindata.password===this.Data[i].Password){
-            this.router.navigate(['/Home'])
-        }
-        else{
-          this.state = "Incorrect username or password.Please try again."
-        }
-      }
+  //   if(f.get('Username')?.value==""){
+  //     this.state = "please enter your username"
+  //   }
+  //   else if(f.get('Password')?.value==""){
+  //     this.state = "please enter your password"
+  //   }else {
+  //     for(let i = 0;i<this.Data.length;i++){
+  //       if(this.logindata.username === this.Data[i].Student_ID&&this.logindata.password===this.Data[i].Password){
+  //           this.router.navigate(['/Home'])
+  //       }
+  //       else{
+  //         this.state = "Incorrect username or password.Please try again."
+  //       }
+  //     }
 
-    // this.router.navigate(['/Home'])
-   }
+  //   // this.router.navigate(['/Home'])
+  //  }
+
+        this.userservice.login(this.logindata.username, this.logindata.password).pipe(first()).subscribe({
+          next: () => {
+            console.log("User valid")
+            const returnUrl = this.route.snapshot.queryParams['/Home']
+            this.router.navigate(['/Home'])
+          },
+          error: error =>{
+
+          }
+        }
+        )
+
+
   }
 }
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,17 +1,46 @@
+import { User } from './../models/user';
 import { environment } from './../../environments/environment';
 import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map, tap } from 'rxjs';
+import { BehaviorSubject, map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
+  private userSubject: BehaviorSubject<User>;
+  public user: Observable<User>;
 
   constructor(private router:Router, private http: HttpClient) {
-
+    this.userSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('user') || '{}'));
+    this.user = this.userSubject.asObservable();
   }
 
+  public get userValue(): User{
+    return this.userSubject.value;
+  }
 
+  login(username:string, password:string)
+  {
+    // console.log(`${environment.apiUrl}/userdata/${username}/${password}`)
+    // this.http.get<User>(`${environment.apiUrl}/userdata/${username}/${password}`).subscribe(data => {
+    //   if (data)
+    //   {
+    //     console.log('Login!');
+    //     return true;
+    //   }
+
+    //   console.log('Login Failed invalid username or password');
+    //   return false;
+    // })
+    
+    console.log(`${environment.apiUrl}/userdata/${username}/${password}`)
+    return this.http.get<User>(`${environment.apiUrl}/userdata/${username}/${password}`)
+    .pipe(map(user => {
+      localStorage.setItem('user', JSON.stringify(user));
+      this.userSubject.next(user);
+    }))
+
+  }
+}
