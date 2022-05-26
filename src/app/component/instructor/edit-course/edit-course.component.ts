c9642649cab1abbad772866b30a5775fd76cbc79
Puchite <puchitekub12@gmail.com>
2022-05-26 08:47:54 +0700

    edit-course

--- src/app/component/instructor/edit-course/edit-course.component.ts
+++ src/app/component/instructor/edit-course/edit-course.component.ts
@@ -1,3 +1,5 @@
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { UserService } from './../../../service/user.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditCourseComponent implements OnInit {
 
-  constructor() { }
+  corseData: any;
+  submitform: any;
+
+  constructor(
+    private userservice:UserService,
+    private fb:FormBuilder
+  ) {
+    this.submitform = this.fb.group({
+      course_id: ['040613102'],
+      course_name: ['']
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit(f:FormGroup)
+  {
+    let course_id = f.get('course_id')?.value;
+    let new_course_name = f.get('course_name')?.value;
+
+    this.userservice.loadSingleCourse(course_id).subscribe(data => {
+      this.corseData = data;
+      if(confirm('รายวิชาที่ต้องการแก้ไขคือ '+data[0].Course_Name+' แก้ไขเป็น '+new_course_name))
+      {
+        this.userservice.changeCourse(course_id, new_course_name).subscribe(
+        { next: () => {
+          alert("แก้ไขรายวิชาเสร็จสิ้น");
+        },
+          error: () => {
+            alert("มีบางอย่างผิดพลาดลองใหม่อีกครั้ง")
+          }
+
+        });
+      }
+    })
+
+    console.log(this.corseData);
+
+
+  }
 }
